b5b19ad901372fa029033dd017af567b26d82d56
BharathiRanga <30995842+BharathiRanga@users.noreply.github.com>
2017-08-18 21:25:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,9 @@
-console.log('Loaded!');
-var img = document.getElementById("madi");
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + "px";
+var button = document.getElementByID("counter");
+var counter = 0;
+button.onClick = function(){
     
-}
-
-img.onclick = function() {
-  
-  var interval = setInterval(moveRight, 50);
+ counter = counter + 1;
+ var span = getElementByID("count");
+ span.innerHTML = counter.toString();
     
 };
