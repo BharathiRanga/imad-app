d481bbddbb7ebd499d4c32dfd29c496aa119c26d
BharathiRanga <30995842+BharathiRanga@users.noreply.github.com>
2017-08-18 21:13:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,12 +74,12 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-
+  
+var counter = 0;
 app.get('/counter', function(req, res){
-    var counter = 0;
+
  counter = counter + 1;
- res.send(counter, toString());
+ res.send("How are you");
     
 });
 
