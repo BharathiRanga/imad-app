343751a0cf98edbffa90b277e7cedb5b5baabc4b
BharathiRanga <30995842+BharathiRanga@users.noreply.github.com>
2017-08-18 20:59:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,4 @@
 console.log('Loaded!');
-var element = document.getElementById("main-text");
-element.innerHTML = "New value";
-
 var img = document.getElementById("madi");
 var marginLeft = 0;
 function moveRight(){
