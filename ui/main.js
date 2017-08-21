4e62cd28bb3a985d8e912f8bd0cb35f839246d88
BharathiRanga <30995842+BharathiRanga@users.noreply.github.com>
2017-08-21 06:17:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,7 @@ button.onclick = function(){
  };
  
  // Make a request 
- request.open("GET","http://bharathiranga.imad.hasura-app.io", true);
+ request.open("GET","http://bharathiranga.imad.hasura-app.io/counter", true);
  request.send(null);
     
 };
