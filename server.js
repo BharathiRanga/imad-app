70283499c53b0081268d6d0a1dfb81ed6784967a
BharathiRanga <30995842+BharathiRanga@users.noreply.github.com>
2017-08-21 11:23:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,8 +100,10 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
 var names = [];
-app.get('/ui/submit-name/name', function(req, res){
+
+app.get('/submit-name/name', function (req, res) {
    var name = req.params.name;
    names.push(name);
    res.send(JSON.stringify(names));
