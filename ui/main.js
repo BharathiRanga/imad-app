0e8740441c9e50ca000675fe8edba6fd9dc5947c
BharathiRanga <30995842+BharathiRanga@users.noreply.github.com>
2017-08-21 11:14:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -41,14 +41,14 @@ button.onclick = function(){
      
      
      // Capture a list of names and render it as a list
-     alert("Button has been clicked");
+  
      var names = ["name1","name2","name3","name4"];
      var list = "";
      for (var i=0; i < names.length; i++) {
          list = list + "<li>"+ names[i] + "</li>";
          
      }
-     alert(list);
+     
      var ul = document.getElementById("nameList");
      ul.innerHTML = list;
      
